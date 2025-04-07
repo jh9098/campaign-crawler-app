4e90898d5f85cc85adee6fc4b641036f338115eb
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-04-07 22:06:33 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-// frontend/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ Netlify에선 꼭 필요
-})
+  base: './',
+});
