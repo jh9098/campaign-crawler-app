9da3bbc35b0cd24544663a0493f45eb31323e338
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-04-07 22:53:32 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
