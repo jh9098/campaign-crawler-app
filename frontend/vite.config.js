efca53c9736e25e61f1f383e19e97e9b6118ef1d
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-04-07 22:20:08 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'public/index.html'), // ✅ 명시적으로 entry 지정
+    },
+  },
 });
