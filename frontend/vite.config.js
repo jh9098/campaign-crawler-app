09ea164f7645c6b2ebff94228507112713600fc9
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-04-08 00:03:58 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  root: ".", // frontend 기준일 경우 기본값
-  publicDir: "public",
   plugins: [react()],
   build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: path.resolve(__dirname, "public/index.html") // ✅ 명시적으로 entry 지정
-    }
+    outDir: "dist"
   }
 });
