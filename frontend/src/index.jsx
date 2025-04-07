4d196683fd6d72dc1087789cc697b7744278d5fa
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-04-08 02:59:37 +0900

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Result from "./Result";
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
