453a79ccde74e6eecbbfd8abd4986380a55128b2
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-04-09 00:11:36 +0900

    Update crawler.py

--- backend/crawler.py
+++ backend/crawler.py
@@ -122,8 +122,8 @@ def run_crawler(session_cookie, selected_days, exclude_keywords):
     if not public_campaigns:
         return [], []
 
-    start_id = 40000
-    end_id = max(public_campaigns) + 100
+    start_id = 39500
+    end_id = 40000#max(public_campaigns) + 100
 
     hidden = []
     public = []
